a8d44851bd4d96d254f2101b6a362022d4c7b3c3
Xing Li <anna.xing.li@gmail.com>
2017-07-21 15:04:50 -0700

    Added Newsbite

--- src/components/newsbite.vue
+++ src/components/newsbite.vue
@@ -1,30 +1,35 @@
 <template>
+
   <div class="content newsbite">
     <div>
-      Placeholder for Newsbite Image
+      <figure class="image">
+      <img src="../assets/newsbite/title.jpg">
+    </figure>
     </div>
     <section class="section">
       <h1 class="title has-text-centered is-2">Project Intro</h1>
+      <h2 class="title is-4">The Company</h2>
       <p>
-        NewsBite is a platform that uses AI technology to extract neutral statements from man Ay sources of news articles, then compose and edit to an unbiased story. The Goal is to create a new platform for people to engage with. It is committed to to provide people with the neutral, comprehensive, and reliable news.
-        <br><br>
-        The situation of Newsbite at that time was —— they have no front end designed at all, but have a general idea and backend built. 
+        NewsBite is a platform that uses AI technology to extract neutral statements from man AI sources of news articles, then compose and edit them into an unbiased story. The goal is to create a new platform for people to engage with. It is committed to provide people with neutral, comprehensive, and reliable news.
       </p>
-      <hr>
+      <br>
+     
       <article>
         <h2 class="title is-4">The Opportunity</h2>
+        <p><strong>Platform:</strong> Responsive Website, Mobile first</p>
         <ul>
-          <li>Responsive Web Interface Design</li>
-          <li>Define Visual Brand</li>
+          <li>NewsBite was a product that had no front end designed, but had a general idea and backend built.</li>
+          <li>They wanted us to design their <strong>responsive web interface</strong> and define their <strong>visual brand</strong>.</li>
         </ul>
         <br>
-        <div class="columns">
-          <div class="column is-two-thirds">
+
+          <h2 class="title is-4">My Role</h2>
             <ul>
-              <p><strong>My Role:</strong></p>
-              <li>I worked closely with 5 other designers. I was highly involved in Lo-Fi wireframes design, Interaction Design, and Branding</li>
+              <li>Create user survey questions</li>
+              <li>Conduct user research, then analyze and collate the research results</li>
+              <li>Highly involved in the Lo-Fi wireframes design, interaction design, and branding design</li>
+              <li>Design responsive web pages</li>
             </ul>      
-          </div>
           <!-- <div class="column">
             <p><strong>Tools</strong></p>
             <ul>
@@ -33,7 +38,7 @@
               <li>Zeplin</li>
             </ul>  
           </div> -->
-        </div>
+
         <!-- <div>
           <p><strong>Time</strong></p>
           <ul>
@@ -59,9 +64,9 @@
       <br>
       
       <article>
-        <h2 class="title is-4">Research</h2>
+        <h2 class="title is-3">Research</h2>
         <h3 class="title is-5">User Research</h3>
-        <p>The primary target users of Newsbite are - Young professionals who consume news digitally every day. We interviewed 7 people who fit this user base to better understand their behaviors, expections, and insight when consume news.    
+        <p>The <strong>primary target users</strong> of NewsBite are - <strong>young professionals who consume news digitally every day</strong>. We interviewed 7 people who fit this user base to better understand their behaviors, expectations, and insights when consuming news.  
         </p>
         <div class="columns">
           <div class="column is-5 is-offset-1">
@@ -73,32 +78,264 @@
         </div>
         <br>
         <h3 class="title is-5">Competitors Learning</h3>
-        <p>We learned from both clients and interviews that these news platforms make people feel authoritative and trustworthy. We analyzed these sites to understand existing models and also for inspirations. These websites include but now limited to NEW York times, BBC, CNN.    
+        <p>We learned from both clients and interviews that these news platforms make people feel authoritative and trustworthy. We analyzed these sites to understand the existing models and also for inspiration. These websites include but were limited to the NEW York times, BBC, and CNN.    
         </p>
       </article>
       
       <br><br><br>
       
       <article>
-        <h2 class="title is-4">Ideation</h2>
+        <h2 class="title is-3">Ideation</h2>
         <h3 class="title is-5">Design Studio</h3>
-        <p>In order to make the low fidelity prototypes, we went through a Design Studio. We sketched different ideas on paper, then discuss and vote to pick the best ideas.    
+        <p>To make the Lo-Fi prototypes, we went through a Design Studio. We sketched different ideas on paper, then discussed and voted to pick the best ideas.   
+        </p>
+        <div class="columns">
+              <div class="column">
+                <figure class="image"><img src="../assets/newsbite/newsbite_design_studio_1.jpg" alt="Design Studio"></figure>
+              </div>
+              <div class="column">
+                <figure class="image"><img src="../assets/newsbite/newsbite_design_studio_2.jpg" alt="Design Studio"></figure>
+              </div>
+              <div class="column">
+                <figure class="image"><img src="../assets/newsbite/newsbite_design_studio_3.jpg" alt="Design Studio"></figure>
+              </div>
+          </div>
+
+          <div class="columns">
+              <div class="column">
+                <p><strong>Divergence Process:</strong></p>
+                <ul>
+                  <li>Individuals ideated on wireframes based on learning from user interviews and competitor news platforms.</li>
+                  <li>Two rounds of presenting ideas to team and acquiring feedback.</li>
+                </ul>
+              </div>
+              <div class="column">
+                <p><strong>Convergence Process:</strong></p>
+                <ul>
+                  <li>Converge on ideas in whiteboard UI sketches</li>
+                  <li>Present ideas to team and get feedback</li>
+                  <li>Integrate on wireframes and consolidate into master sketch files.</li>
+                </ul>
+              </div>
+            </div>
+      </article><br><br><br>
+      
+      <article>
+        <h2 class="title is-3">Lo-Fi Phase</h2>
+        <h3 class="title is-4">Lo-Fi Wireframes</h3>
+        <img src="../assets/newsbite/lo_fi_wireframes.png">
+        <h3 class="title is-4">Interaction Design</h3>
+        <p>Our goal is to let users intuitively realize that this is different from traditional news platforms, articles they can read on NewsBite are neutral and provide different perspectives of topics. We encountered several challenges, then we explored different possibilities to show the concept.         
+        </p>
+        <h3 class="title is-5">Home Page</h3>
+        <p>How does one allow people to learn the purpose of this website when they land on the homepage?</p>
+
+        <div class="columns">
+          <div class="column is-2 is-offset-4">
+            <img src="../assets/newsbite/lo_fi_interaction_home_1.gif">
+          </div>
+          <div class="column is-2">
+            <img src="../assets/newsbite/lo_fi_interaction_home_2.gif">
+          </div>
+        </div>
+
+        <p>1. Homepage has a neutral story title; Transition page has all three titles of the same story.2.  For every story, people can see the titles from all different angles from the home page.................. break to two
+        </p>
+
+        <h3 class="title is-5">Article page</h3>
+        <p>
+          <ul>
+            <li>How to alert people that there also is two other sides of a story besides the neutral story?</li>
+            <li>How to notify people which side of the story they are viewing?</li>
+          </ul>
+        </p>
+
+        <div class="columns">
+          <div class="column is-2 is-offset-4">
+            <img src="../assets/newsbite/lo_fi_interaction_article_1.gif">
+          </div>
+          <div class="column is-2">
+            <img src="../assets/newsbite/lo_fi_interaction_article_2.gif">
+          </div>
+        </div>
+
+        <p>1. Triangles at the bottom of the page show which perspective is being viewed. (Use triangle means the three sides are equal).2.  Three layers as different sides of the story. Colors at the corner show which side is being viewed................... break to two
+        </p>
+      
+        <h3 class="title is-4">Branding Exploration</h3>
+          <p>We encountered some issues we did not expect before we started the branding exercise. NewsBite has two co-founders. When we communicated with them about their expectation of branding, we received different opinions from each of them. For example, the same car, Founder A thinks is “classic”, but founder B thinks it is “too old”. So, we decided to have a “Branding Workshop” to finalize the direction we all agreed to proceed.
+          </p>
+           <div class="columns"> 
+            <div class="column">
+                <figure class="image"><img src="../assets/newsbite/branding_1.jpg" alt="branding"></figure>
+            </div>
+          </div>
+          <p>We prepared about 80 pictures with various random objects. (Car, bike lamp, cup, brands, etc.). In the end, we used their consensus to create the word cloud of “Like” and “Dislike”, and we obtained these important branding references, as shown in the picture below.
+          </p>
+          <div class="columns">
+              <div class="column">
+                <figure class="image"><img src="../assets/newsbite/branding_2.png" alt="branding"></figure>
+              </div>
+              <div class="column">
+                <figure class="image"><img src="../assets/newsbite/branding_3.png" alt="branding"></figure>
+              </div>
+          </div>
+      </article><br><br><br>
+      
+      <article>
+        <h2 class="title is-3">Hi-Fi Phase</h2>
+        <h3 class="title is-4">Font Type Exploration</h3>
+        <p>
+          We are very clear what type of fonts we are looking for: High-definition, easy to read, timeless, and match the brand image. We compared many fonts.
+        </p>
+        <img src="../assets/newsbite/hi_fi_font.png">
+         <p>
+          When we were working on colors, we first researched and analyzed some brands people think are “trustworthy”, as well as colors of some existing news platforms. Then, we combined and compared them with the brand image, to reveal the following:
+        </p>
+          ---------------need to be edit--------------
+        
+        <h3 class="title is-4">Logo Exploration</h3>
+        <p>
+          We worked as a team to explore the logo design. We tried to make this logo simple, identifiable, also can shoe display NewsBite a platform to provide news with a new concept.
         </p>
         <div class="columns">
           <div class="column is-5 is-offset-1">
-            <figure class="image"><img src="../assets/newsbite/design_process/research_stickynotes.jpg"></figure>
+            <img src="../assets/newsbite/hi_fi_logo_1.jpg">
           </div>
           <div class="column is-5">
-            <img src="../assets/newsbite/design_process/research_user_goals.png">
+            <img src="../assets/newsbite/hi_fi_logo_2.jpg">
           </div>
         </div>
-        <br>
-        <h3 class="title is-5">Competitors Learning</h3>
-        <p>We learned from both clients and interviews that these news platforms make people feel authoritative and trustworthy. We analyzed these sites to understand existing models and also for inspirations. These websites include but now limited to NEW York times, BBC, CNN.    
+
+      </article><br><br><br>
+
+
+    <article>
+        <h1 id="Praiseworthy-Design-Process" class="title has-text-centered is-2">The Solution</h1>
+
+        <h2 class="title is-3">Home Page</h2>
+
+        <div class="columns">
+          <div class="column is-4 is-offset-2">
+            <img src="../assets/newsbite/solution_home_desktop.gif">
+          </div>
+          <div class="column is-4">
+            <img src="../assets/newsbite/solution_home_mobile.gif">
+          </div>
+        </div>
+
+        <h2 class="title is-3">Article Page</h2>
+        <p>
+          We decided not to use words or color labels to represent which side of the story is being viewed, in case the user’s subconscious guides their thoughts. For example: "Conservative/Liberal" "Negative/positive" "Red color (Wrong)/Green(Right)".<br>
+          <strong>Desktop</strong> —— Taps on the top show the current page and which perspective is being viewed.<br>
+          <strong>Mobile</strong> —— NewsBite’s logo deforms into a cube when swiping to the left and right.
         </p>
-      </article>
-      
-      
+
+        <div class="columns">
+          <div class="column is-4 is-offset-2">
+            <img src="../assets/newsbite/solution_article_desktop.gif">
+          </div>
+          <div class="column is-4">
+            <img src="../assets/newsbite/solution_article_mobile.gif">
+          </div>
+        </div>
+
+
+          <h2 class="title is-3">Other Pages</h2>
+            <p class="has-text-centered">Desktop</p>
+            <div class="columns">
+              <div class="column">
+                <figure class="image"><img src="../assets/newsbite/solution_desktop_bookmarked.png" alt="other pages"></figure>
+              </div>
+              <div class="column">
+                <figure class="image"><img src="../assets/newsbite/solution_desktop_following.png" alt="other pages"></figure>
+              </div>
+              <div class="column">
+                <figure class="image"><img src="../assets/newsbite/solution_desktop_profile.png" alt="other pages"></figure>
+              </div>
+              <div class="column">
+                <figure class="image"><img src="../assets/newsbite/solution_desktop_sign_in.png" alt="other pages"></figure>
+              </div>
+              <div class="column">
+                <figure class="image"><img src="../assets/newsbite/solution_desktop_sign_up.png" alt="other pages"></figure>
+              </div>
+            </div>
+
+            <p class="has-text-centered">Mobile</p>
+            <div class="columns">
+              <div class="column">
+                <figure class="image"><img src="../assets/newsbite/solution_mobile_bookmarked.png" alt="other pages"></figure>
+              </div>
+              <div class="column">
+                <figure class="image"><img src="../assets/newsbite/solution_mobile_following.png" alt="other pages"></figure>
+              </div>
+              <div class="column">
+                <figure class="image"><img src="../assets/newsbite/solution_mobile_profile.png" alt="other pages"></figure>
+              </div>
+              <div class="column">
+                <figure class="image"><img src="../assets/newsbite/solution_mobile_sign_in.png" alt="other pages"></figure>
+              </div>
+              <div class="column">
+                <figure class="image"><img src="../assets/newsbite/solution_mobile_sign_up.png" alt="other pages"></figure>
+              </div>
+            </div>
+
+
+        <h2 class="title is-3">Branding</h2>
+        <p>
+          We decided not to use words or color labels to represent which side of the story is being viewed, in case the user’s subconscious guides their thoughts. For example: "Conservative/Liberal" "Negative/positive" "Red color (Wrong)/Green(Right)".<br>
+          <strong>Desktop</strong> —— Taps on the top show the current page and which perspective is being viewed.<br>
+          <strong>Mobile</strong> —— NewsBite’s logo deforms into a cube when swiping to the left and right.
+        </p>
+
+          <h3 class="title is-4">Font Type</h3>
+          <p>
+            Title: Charter, Body “Franklin Gothic”
+          </p>
+            <div class="columns">
+              <div class="column is-half is-offset-one-quarter">
+                <img src="../assets/newsbite/branding_font.png">
+              </div>
+            </div>
+
+          <h3 class="title is-4">Color</h3>
+          <p>
+            We identified “Teal” as the main brand color. “Teal” means “Communication”, “Trustworthiness”, “Reliable”, and “clarity of thought” in Psychology. Also, because “Teal” is not a common color, it makes this brand more identifiable and different from other existing news platforms.
+          </p>
+          <div class="columns">
+              <div class="column is-half is-offset-one-quarter">
+                <img src="../assets/newsbite/branding_color.png">
+              </div>
+            </div>
+
+          <h3 class="title is-4">Logo</h3>
+           <p>
+            The final logo is a combination of three rectangles, it shows that NewsBite aims to deliver news in a new concept, but also reflects the different sides of the same story are equal to each other.
+          </p>
+
+            <div class="columns">
+              <div class="column is-half is-offset-one-quarter">
+                <img src="../assets/newsbite/branding_logo.png">
+              </div>
+            </div>
+
+          <br><br><br>
+
+        <article>
+        <h1 id="Praiseworthy-Design-Process" class="title has-text-centered is-2">The Final Proposal</h1>
+        <p>
+          Throughout the design process, we maintained very close communication with our customers, continued to explore and perform changes based on their feedback. We have successfully delivered this project to our customers where both our team and their customers are satisfied with the final results. Our team and our customer all believe that the final work has succeeded in accomplishing the original design principles - Solid, Trustworthy, Clean, Neutral, and Authoritative. 
+
+        </p>
+        </article>
+
+
+    </article>
+
+
+
+
+
     </section>
   </div>
 </template>
